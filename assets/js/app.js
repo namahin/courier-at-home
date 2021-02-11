9f6bfe115cc7b6cd7052824a619a55bc1fd71cb2
Noore Alam Mahin <50703361+namahin@users.noreply.github.com>
2021-02-11 16:14:15 +0600

    Registration page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,20 @@ $('input,textarea').focus(function () {
 }).blur(function () {
     $(this).attr('placeholder', $(this).data('placeholder'));
 });
+
+// Clients Carousel slider js
+$('.clients-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:6
+        }
+    }
+});
