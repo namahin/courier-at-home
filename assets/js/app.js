f204f34de6fa6c50767beb0b271b4e9c9927df39
Noore Alam Mahin <50703361+namahin@users.noreply.github.com>
2021-03-29 19:58:12 +0600

    update style sheet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+// active wow js
+new WOW().init();
+
 // remove placeholder on focus
 $('input,textarea').focus(function () {
     $(this).data('placeholder', $(this).attr('placeholder'))
@@ -8,17 +11,52 @@ $('input,textarea').focus(function () {
 
 // Clients Carousel slider js
 $('.clients-carousel').owlCarousel({
-    loop:true,
-    margin:15,
-    responsive:{
-        0:{
-            items:3
+    loop: true,
+    margin: 15,
+    responsive: {
+        0: {
+            items: 3
         },
-        600:{
-            items:6
+        600: {
+            items: 6
         },
-        1000:{
-            items:6
+        1000: {
+            items: 6
         }
     }
+});
+
+
+// counter on Scroll
+var counted = 0;
+$(window).scroll(function () {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (counted == 0 && $(window).scrollTop() > oTop) {
+        $('.count').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
+
+                });
+        });
+        counted = 1;
+    }
+
 });
